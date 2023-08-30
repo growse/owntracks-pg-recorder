cd54f6f50e743e5ce9bd58d5521edd6487e935fd
Andrew Rowson <github@growse.com>
2023-08-30 10:53:30 +0100

    Inaccurate locations screen should do one before and after

--- location.go
+++ location.go
@@ -460,27 +460,45 @@ type InaccurateLocation struct {
 
 func (env *Env) GetInaccurateLocationPoints(c *gin.Context) {
 	query := `
-SELECT
-    id,
-    devicetimestamp,
-    accuracy,
-    concat(st_y (st_astext (point)), ',', st_x (st_astext (point))) as latlng,
-    coalesce(geocoding -> 'results' -> 0 ->> 'formatted_address', '') as address,
-    st_distance (locations.point, lag(locations.point, 1, locations.point) OVER (ORDER BY locations.devicetimestamp)) AS distance,
-    coalesce(
-       3.6 * ST_Distance (point, lag(point, 1, point) OVER (ORDER BY devicetimestamp ASC)) /
-              (extract('epoch' FROM (devicetimestamp - lag(devicetimestamp) OVER (ORDER BY devicetimestamp ASC)))+1),
-       0
-    ) AS speed
-FROM
-    locations
-WHERE
-    devicetimestamp >= '2018-01-01'
-    AND devicetimestamp < '2025-10-02'
-ORDER BY
-    speed DESC
-LIMIT 20;
+SELECT id,
+       devicetimestamp,
+       accuracy,
+       concat(st_y(st_astext(point)), ',', st_x(st_astext(point)))                                     as latlng,
+       coalesce(geocoding -> 'results' -> 0 ->> 'formatted_address', '')                               as address,
+       st_distance(locations.point,
+                   lag(locations.point, 1, locations.point) OVER (ORDER BY locations.devicetimestamp)) AS distance,
+       coalesce(
+                       3.6 * ST_Distance(point, lag(point, 1, point) OVER (ORDER BY devicetimestamp ASC)) /
+                       (extract('epoch' FROM
+                                (devicetimestamp - lag(devicetimestamp) OVER (ORDER BY devicetimestamp ASC))) + 1),
+                       0
+           )                                                                                           AS speed
+FROM locations
+WHERE id in (with ids as (SELECT id,
+                                 lag(id) over (order by locations.devicetimestamp desc)  as prev,
+                                 lead(id) over (order by locations.devicetimestamp desc) as next,
+                                 coalesce(
+                                                 3.6 * ST_Distance(point,
+                                                                   lag(point, 1, point) OVER (ORDER BY devicetimestamp ASC)) /
+                                                 (extract('epoch' FROM (devicetimestamp -
+                                                                        lag(devicetimestamp) OVER (ORDER BY devicetimestamp ASC))) +
+                                                  1),
+                                                 0
+                                     )                                                   AS speed
+                          FROM locations
+                          ORDER BY speed DESC
+                          LIMIT 20)
 
+             select ids.id
+             from ids
+             union
+             SELECT ids.next
+             from ids
+             union
+             select ids.prev
+             from ids)
+ORDER BY id DESC
+;
 `
 	rows, err := env.db.Query(query)
 	if err != nil {
